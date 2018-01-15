9397aa89642c10b89a42c17165ce9b0cd0752949
Michael Seifarth <michael@bynder.com>
2018-01-15 17:24:09 +0100

    git init

--- app/containers/NotFoundPage/Loadable.js
+++ app/containers/NotFoundPage/Loadable.js
@@ -1,6 +1,11 @@
 /**
  * Asynchronously loads the component for NotFoundPage
  */
-import loadable from 'loadable-components';
+import Loadable from 'react-loadable';
 
-export default loadable(() => import('./index'));
+import LoadingIndicator from 'components/LoadingIndicator';
+
+export default Loadable({
+  loader: () => import('./index'),
+  loading: LoadingIndicator,
+});
