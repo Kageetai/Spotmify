889807344c62820dbe04b11aaa9648c0f2f26e22
Michael Seifarth <michael@bynder.com>
2018-01-24 22:54:29 +0100

    declutter HomePage

--- app/global-styles.js
+++ app/global-styles.js
@@ -25,12 +25,6 @@ const GlobalStyle = createGlobalStyle`
     min-height: 100%;
     min-width: 100%;
   }
-
-  p,
-  label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
-    line-height: 1.5em;
-  }
 `;
 
 export default GlobalStyle;
